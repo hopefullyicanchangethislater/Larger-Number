5060ef15c1c3815fbbb6693ee8d8c1d97e797537
hopefullyicanchangethislater <121258893+hopefullyicanchangethislater@users.noreply.github.com>
2023-02-15 11:50:57 -0500

    Add project files.

---
+++ Larger Number/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// This picks the larger of 2 predefined numbers. 
+// This could easily be used as a way of deciding some kind of alert
+// like money left or payment still due by having the values compared
+// be generated elsewhere.
+
+// Really powerful stuff...
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
